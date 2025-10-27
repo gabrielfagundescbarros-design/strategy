59432703e2a035c0e02a6b489ffa8f2d7542b586
Fags <gabrielfagundescbarros@gmail.com>
2025-10-26 22:07:21 -0300

    Implementando métodos de "Disciplina"

--- src/main/java/com/strategy/Disciplina.java
+++ src/main/java/com/strategy/Disciplina.java
@@ -22,9 +22,46 @@ public class Disciplina {
      * Calcula a média usando a estratégia injetada
      */
     public void calcularMedia() {
-        // TODO: Implementar usando estrategia.calcularMedia()
-        // TODO: Implementar usando estrategia.verificarSituacao()
+        // Implementar usando estrategia.calcularMedia()
+        this.media = estrategia.calcularMedia(p1, p2);
+        // Implementar usando estrategia.verificarSituacao()
+        this.situacao = estrategia.verificarSituacao(media);
     }
-    
-    // TODO: Implementar getters e setters
+
+    // getters e setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getP1() {
+        return p1;
+    }
+
+    public void setP1(double p1) {
+        this.p1 = p1;
+    }
+
+    public double getP2() {
+        return p2;
+    }
+
+    public void setP2(double p2) {
+        this.p2 = p2;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setEstrategia(MediaStrategy estrategia) {
+        this.estrategia = estrategia;
+    }
 }
